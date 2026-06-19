4dc7c687ec55bd9ce17729b17f888ec23e07e5f8
Dan <danyridolfo98@gmail.com>
2026-06-19 15:27:47 +0200

    Added defragmentation

--- srcs/free.c
+++ srcs/free.c
@@ -6,13 +6,16 @@ static void	coalesce(t_block *block)
 	{
 		block->size += sizeof(t_block) + block->next->size;
 		block->next = block->next->next;
+
 		if (block->next)
 			block->next->prev = block;
 	}
+
 	if (block->prev && block->prev->free)
 	{
 		block->prev->size += sizeof(t_block) + block->size;
 		block->prev->next = block->next;
+
 		if (block->next)
 			block->next->prev = block->prev;
 	}
@@ -26,20 +29,25 @@ void	free(void *ptr)
 
 	if (!ptr)
 		return ;
+
 	zone = find_zone(ptr, &list);
 	if (!zone)
 		return ;
+
 	block = (t_block *)ptr - 1;
 	if (block->free)
 		return ;
+
 	block->free = 1;
 	if (list == &g_heap.large)
 	{
 		free_zone(list, zone);
 		return ;
 	}
+
 	zone->free += block->size + sizeof(t_block);
 	coalesce(block);
+
 	if (zone->free == zone->size - sizeof(t_zone))
 		free_zone(list, zone);
 }
--- srcs/realloc.c
+++ srcs/realloc.c
@@ -9,9 +9,11 @@ static int	try_grow_inplace(t_block *block, size_t old_size, size_t aligned, t_z
 
 	if (!block->next || !block->next->free)
 		return (0);
+
 	next_total = sizeof(t_block) + block->next->size;
 	if (old_size + next_total < aligned)
 		return (0);
+
 	remainder = old_size + next_total - aligned;
 	if (remainder > sizeof(t_block) + 16)
 	{
@@ -22,6 +24,7 @@ static int	try_grow_inplace(t_block *block, size_t old_size, size_t aligned, t_z
 		split->next = block->next->next;
 		if (split->next)
 			split->next->prev = split;
+
 		block->next = split;
 		block->size = aligned;
 		zone->free -= aligned - old_size;
@@ -32,6 +35,7 @@ static int	try_grow_inplace(t_block *block, size_t old_size, size_t aligned, t_z
 		block->next = block->next->next;
 		if (block->next)
 			block->next->prev = block;
+
 		zone->free -= next_total;
 	}
 	return (1);
@@ -48,32 +52,41 @@ void	*realloc(void *ptr, size_t size)
 
 	if (!ptr)
 		return (malloc(size));
+
 	if (size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
+
 	block = (t_block *)ptr - 1;
 	old_size = block->size;
 	aligned = ALIGN16(size);
 	if (aligned == old_size)
 		return (ptr);
+
 	if (aligned < old_size)
 	{
+
 		if ((old_size <= TINY_MAX && aligned <= TINY_MAX)
 			|| (old_size <= SMALL_MAX && aligned <= SMALL_MAX))
 			return (ptr);
 	}
+
 	if (aligned > old_size && aligned <= SMALL_MAX)
 	{
 		zone = find_zone(ptr, &list);
+
 		if (zone && list != &g_heap.large
 			&& try_grow_inplace(block, old_size, aligned, zone))
 			return (ptr);
 	}
+
 	new_ptr = malloc(size);
+
 	if (!new_ptr)
 		return (NULL);
+
 	ft_memcpy(new_ptr, ptr, old_size < aligned ? old_size : aligned);
 	free(ptr);
 	return (new_ptr);
